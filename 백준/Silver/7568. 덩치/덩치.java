352d6d7f09d18aa7dcbc7c1edfbf32a9a83b0d47
Sj-jun <aa1712@naver.com>
2025-12-09 13:41:58 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 15796 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        String[] a;
+        for(int i=0; i < N; i++){
+            a = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(a[0]);
+            arr[i][1] = Integer.parseInt(a[1]);
+        }
+        
+        for(int i = 0; i < N; i++) {
+			int rank = 1;
+			
+			for(int j = 0; j < N; j++) {
+				if(i == j) continue;
+				if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+					rank++;
+				}
+			}
+ 
+			System.out.print(rank + " ");
+		}
+ 
+
+    }
+}
