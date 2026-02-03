7d8b368b61ac667cf3fc81ca7d7649ec1ff1f20a
Sj-jun <aa1712@naver.com>
2026-02-03 14:56:23 +0900

    [level 1] Title: 폰켓몬, Time: 16.31 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int type = map.size();
+        int pick = nums.length / 2;
+        
+        return Math.min(type, pick);
+    }
+}
+    
