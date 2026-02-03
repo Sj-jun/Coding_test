c4661bf7bd21b11101b37256b485bb689d10b641
Sj-jun <aa1712@naver.com>
2026-02-03 14:17:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.00 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,27 +1,23 @@
 import java.util.*;
-
 class Solution {
     public String solution(String[] participant, String[] completion) {
-
+        
         HashMap<String, Integer> map = new HashMap<>();
-
-        // 참가자 카운트 +1
-        for (String name : participant) {
+        
+        for(String name : participant){
             map.put(name, map.getOrDefault(name, 0) + 1);
+            
         }
-
-        // 완주자 카운트 -1
-        for (String name : completion) {
+        
+        for(String name : completion){
             map.put(name, map.get(name) - 1);
         }
-
-        // 값이 0이 아닌 사람이 완주 못한 선수
-        for (String key : map.keySet()) {
-            if (map.get(key) != 0) {
+        
+        for(String key : map.keySet()){
+            if(map.get(key) != 0){
                 return key;
             }
         }
-
         return "";
     }
 }
